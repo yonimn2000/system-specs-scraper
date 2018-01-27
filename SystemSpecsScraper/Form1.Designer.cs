4698c09f8b07a0d389f56f7a6f562fbe11aa96e2
Jonathan <yonimn2000@gmail.com>
2018-01-27 18:51:41 -0500

    Works

--- SystemSpecsScraper/Form1.Designer.cs
+++ SystemSpecsScraper/Form1.Designer.cs
@@ -28,12 +28,166 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.OutputTable = new System.Windows.Forms.DataGridView();
+            this.HostNamesList = new System.Windows.Forms.RichTextBox();
+            this.ReadHostsBTN = new System.Windows.Forms.Button();
+            this.GetSpecsBTN = new System.Windows.Forms.Button();
+            this.FailedComputersList = new System.Windows.Forms.RichTextBox();
+            this.CopyTableBTN = new System.Windows.Forms.Button();
+            this.DomainLBL = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ClearTableBTN = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            ((System.ComponentModel.ISupportInitialize)(this.OutputTable)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // OutputTable
+            // 
+            this.OutputTable.AllowUserToAddRows = false;
+            this.OutputTable.AllowUserToDeleteRows = false;
+            this.OutputTable.AllowUserToOrderColumns = true;
+            this.OutputTable.AllowUserToResizeRows = false;
+            this.OutputTable.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.OutputTable.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.OutputTable.ClipboardCopyMode = System.Windows.Forms.DataGridViewClipboardCopyMode.EnableAlwaysIncludeHeaderText;
+            this.OutputTable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.OutputTable.Location = new System.Drawing.Point(324, 27);
+            this.OutputTable.Name = "OutputTable";
+            this.OutputTable.ReadOnly = true;
+            this.OutputTable.RowHeadersVisible = false;
+            this.OutputTable.Size = new System.Drawing.Size(648, 222);
+            this.OutputTable.TabIndex = 0;
+            // 
+            // HostNamesList
+            // 
+            this.HostNamesList.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.HostNamesList.Location = new System.Drawing.Point(12, 27);
+            this.HostNamesList.Name = "HostNamesList";
+            this.HostNamesList.Size = new System.Drawing.Size(150, 164);
+            this.HostNamesList.TabIndex = 0;
+            this.HostNamesList.Text = "";
+            // 
+            // ReadHostsBTN
+            // 
+            this.ReadHostsBTN.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.ReadHostsBTN.Location = new System.Drawing.Point(12, 197);
+            this.ReadHostsBTN.Name = "ReadHostsBTN";
+            this.ReadHostsBTN.Size = new System.Drawing.Size(306, 23);
+            this.ReadHostsBTN.TabIndex = 1;
+            this.ReadHostsBTN.Text = "Read Host Names From ";
+            this.ReadHostsBTN.UseVisualStyleBackColor = true;
+            this.ReadHostsBTN.Click += new System.EventHandler(this.ReadHostsBTN_Click);
+            // 
+            // GetSpecsBTN
+            // 
+            this.GetSpecsBTN.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.GetSpecsBTN.Location = new System.Drawing.Point(12, 226);
+            this.GetSpecsBTN.Name = "GetSpecsBTN";
+            this.GetSpecsBTN.Size = new System.Drawing.Size(150, 23);
+            this.GetSpecsBTN.TabIndex = 2;
+            this.GetSpecsBTN.Text = "Get Specs";
+            this.GetSpecsBTN.UseVisualStyleBackColor = true;
+            this.GetSpecsBTN.Click += new System.EventHandler(this.GetSpecsBTN_Click);
+            // 
+            // FailedComputersList
+            // 
+            this.FailedComputersList.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.FailedComputersList.Location = new System.Drawing.Point(168, 27);
+            this.FailedComputersList.Name = "FailedComputersList";
+            this.FailedComputersList.Size = new System.Drawing.Size(150, 164);
+            this.FailedComputersList.TabIndex = 5;
+            this.FailedComputersList.Text = "";
+            // 
+            // CopyTableBTN
+            // 
+            this.CopyTableBTN.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.CopyTableBTN.Location = new System.Drawing.Point(247, 226);
+            this.CopyTableBTN.Name = "CopyTableBTN";
+            this.CopyTableBTN.Size = new System.Drawing.Size(71, 23);
+            this.CopyTableBTN.TabIndex = 3;
+            this.CopyTableBTN.Text = "Copy Table";
+            this.CopyTableBTN.UseVisualStyleBackColor = true;
+            this.CopyTableBTN.Click += new System.EventHandler(this.ExportTableBTN_Click);
+            // 
+            // DomainLBL
+            // 
+            this.DomainLBL.AutoSize = true;
+            this.DomainLBL.Location = new System.Drawing.Point(12, 9);
+            this.DomainLBL.Name = "DomainLBL";
+            this.DomainLBL.Size = new System.Drawing.Size(34, 13);
+            this.DomainLBL.TabIndex = 6;
+            this.DomainLBL.Text = "Hosts";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(165, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Failed Computers";
+            // 
+            // ClearTableBTN
+            // 
+            this.ClearTableBTN.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.ClearTableBTN.Location = new System.Drawing.Point(168, 226);
+            this.ClearTableBTN.Name = "ClearTableBTN";
+            this.ClearTableBTN.Size = new System.Drawing.Size(73, 23);
+            this.ClearTableBTN.TabIndex = 4;
+            this.ClearTableBTN.Text = "Clear Table";
+            this.ClearTableBTN.UseVisualStyleBackColor = true;
+            this.ClearTableBTN.Click += new System.EventHandler(this.ClearTableBTN_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar1.Location = new System.Drawing.Point(324, 9);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(648, 12);
+            this.progressBar1.Step = 1;
+            this.progressBar1.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(984, 261);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.ClearTableBTN);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.DomainLBL);
+            this.Controls.Add(this.CopyTableBTN);
+            this.Controls.Add(this.FailedComputersList);
+            this.Controls.Add(this.GetSpecsBTN);
+            this.Controls.Add(this.ReadHostsBTN);
+            this.Controls.Add(this.HostNamesList);
+            this.Controls.Add(this.OutputTable);
+            this.Name = "Form1";
+            this.Text = "System Specs Scraper";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.OutputTable)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView OutputTable;
+        private System.Windows.Forms.RichTextBox HostNamesList;
+        private System.Windows.Forms.Button ReadHostsBTN;
+        private System.Windows.Forms.Button GetSpecsBTN;
+        private System.Windows.Forms.RichTextBox FailedComputersList;
+        private System.Windows.Forms.Button CopyTableBTN;
+        private System.Windows.Forms.Label DomainLBL;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button ClearTableBTN;
+        private System.Windows.Forms.ProgressBar progressBar1;
     }
 }
 
--- SystemSpecsScraper/Form1.cs
+++ SystemSpecsScraper/Form1.cs
@@ -1,20 +1,161 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
+using System.DirectoryServices;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Management;
+using System.Text.RegularExpressions;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace SystemSpecsScraper
 {
     public partial class Form1 : Form
     {
+        List<WMI_Class> WMI_Classes = new List<WMI_Class>();
+        string domain = System.Net.NetworkInformation.IPGlobalProperties.GetIPGlobalProperties().DomainName;
+
+        struct WMI_Class
+        {
+            public string name;
+            public IDictionary<string, string> properties;
+            public WMI_Class(string newName)
+            {
+                name = newName;
+                properties = new Dictionary<string, string>();
+            }
+        }
+
+        private void InitializeWMI_Classes()
+        {
+            WMI_Class CPU = new WMI_Class("Win32_Processor");
+            CPU.properties.Add("CPU", "Name");
+            WMI_Classes.Add(CPU);
+
+            WMI_Class OS = new WMI_Class("Win32_OperatingSystem");
+            OS.properties.Add("RAM", "TotalVisibleMemorySize");
+            OS.properties.Add("OS", "Caption");
+            OS.properties.Add("OS Version", "Version");
+            WMI_Classes.Add(OS);
+
+            WMI_Class Device = new WMI_Class("Win32_ComputerSystemProduct");
+            Device.properties.Add("Serial Number", "IdentifyingNumber");
+            Device.properties.Add("Model Number", "Name");
+            Device.properties.Add("Vendor", "Vendor");
+            Device.properties.Add("Model Name", "Version");
+            WMI_Classes.Add(Device);
+        }
+
+        private void InitializeOutputTableColumns()
+        {
+            OutputTable.Columns.Add("Date", "Date");
+            OutputTable.Columns.Add("Host", "Host");
+            foreach (WMI_Class wmiOBJ in WMI_Classes)
+            {
+                foreach (KeyValuePair<string, string> property in wmiOBJ.properties)
+                {
+                    OutputTable.Columns.Add(property.Key, property.Key);
+                }
+            }
+        }
+
         public Form1()
         {
             InitializeComponent();
+            this.WindowState = FormWindowState.Maximized;
+            InitializeWMI_Classes();
+            InitializeOutputTableColumns();
+        }
+
+        private void ReadHostsBTN_Click(object sender, EventArgs e)
+        {
+            HostNamesList.Text = string.Join("\n", GetDomainComputers(domain));
+        }
+
+        public static List<string> GetDomainComputers(string domain)
+        {
+            List<string> ComputerNames = new List<string>();
+            DirectoryEntry entry = new DirectoryEntry("LDAP://" + domain);
+            DirectorySearcher mySearcher = new DirectorySearcher(entry);
+            mySearcher.Filter = ("(objectClass=computer)");
+            mySearcher.SizeLimit = int.MaxValue;
+            mySearcher.PageSize = int.MaxValue;
+            foreach (SearchResult resEnt in mySearcher.FindAll())
+            {
+                string ComputerName = resEnt.GetDirectoryEntry().Name;
+                if (ComputerName.StartsWith("CN="))
+                    ComputerName = ComputerName.Remove(0, "CN=".Length);
+                if (!ComputerName.Contains("MediaAdmin"))
+                    ComputerNames.Add(ComputerName);
+            }
+            mySearcher.Dispose();
+            entry.Dispose();
+            return ComputerNames;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ReadHostsBTN.Enabled = !domain.Equals("");
+            ReadHostsBTN.Text = !domain.Equals("") ? "Read Host Names From " + domain + " Domain" : "Not Connected to a Domain";
+        }
+
+        private void GetSpecsBTN_Click(object sender, EventArgs e)
+        {
+            new Thread(() =>
+            {
+                Thread.CurrentThread.IsBackground = true;
+                GetSpecsBTN.Enabled = HostNamesList.Enabled = FailedComputersList.Enabled = ClearTableBTN.Enabled = CopyTableBTN.Enabled = ReadHostsBTN.Enabled = false;
+                GetSpecs();
+                GetSpecsBTN.Enabled = HostNamesList.Enabled = FailedComputersList.Enabled = ClearTableBTN.Enabled = CopyTableBTN.Enabled = ReadHostsBTN.Enabled = true;
+            }).Start();
+        }
+
+        private void GetSpecs()
+        {
+            progressBar1.Value = 0;
+            string[] computerNames = Regex.Replace(HostNamesList.Text, @"^\s+$[\r\n]*", "", RegexOptions.Multiline).Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.None);//Remove empty lines
+            progressBar1.Maximum = computerNames.Count();
+            foreach (string computerName in computerNames)
+            {
+                progressBar1.PerformStep();
+                DataGridViewRow row = OutputTable.Rows[OutputTable.Rows.Add()];
+                try
+                {
+                    foreach (WMI_Class wmiClass in WMI_Classes)
+                    {
+                        ManagementObjectSearcher searcher = new ManagementObjectSearcher("\\\\" + computerName + "\\root\\CIMV2", "SELECT * FROM " + wmiClass.name);
+                        foreach (ManagementObject queryObj in searcher.Get())
+                        {
+                            foreach (KeyValuePair<string, string> property in wmiClass.properties)
+                            {
+                                if (property.Key.Equals("RAM"))
+                                    row.Cells[property.Key].Value = Math.Round(int.Parse(queryObj[property.Value].ToString()) / 1048576.0, 2).ToString();
+                                else
+                                    row.Cells[property.Key].Value = queryObj[property.Value].ToString();
+                            }
+                        }
+                    }
+                    row.Cells["Date"].Value = DateTime.Now;
+                    row.Cells["Host"].Value = computerName;
+                }
+                catch (Exception)
+                {
+                    if (!FailedComputersList.Text.Contains(computerName))
+                        FailedComputersList.AppendText(computerName + Environment.NewLine);
+                }
+                if (row.Cells[0].Value == null)
+                    OutputTable.Rows.RemoveAt(OutputTable.Rows.Count - 1);
+            }
+        }
+
+        private void ExportTableBTN_Click(object sender, EventArgs e)
+        {
+            OutputTable.SelectAll();
+            Clipboard.SetDataObject(OutputTable.GetClipboardContent());
+        }
+
+        private void ClearTableBTN_Click(object sender, EventArgs e)
+        {
+            OutputTable.Rows.Clear();
         }
     }
 }
